f375296211e34045c75add1633efc6f0eddba661
wpine215 <wpine215@gmail.com>
2019-10-20 03:39:02 -0400

    disable pagination

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -152,5 +152,5 @@ STATICFILES_DIRS = ( os.path.join('static'), )
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 0
 }
